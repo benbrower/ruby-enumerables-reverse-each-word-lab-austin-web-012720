1cf87eaacb37594f770d48c6002c273bf7ba7ea3
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 04:19:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   rev_words = []
   norm_words = sentence.split(" ")
   norm_words.each do |word|
-    rev_word << word.reverse
+    rev_word << word.reverse()
   end
   rev_words.join(" ")
 end
@@ -11,7 +11,7 @@ def reverse_each_word(sentence)
   rev_words = []
   norm_words = sentence.split(" ")
   norm_words.collect do |word|
-    rev_word << word.reverse
+    rev_word << word.reverse().
   end
   rev_words.join(" ")
 end
